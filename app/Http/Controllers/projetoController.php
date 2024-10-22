6f8a62606031af74925a64e718e7611c1a3e751e
Leticia Furuse <leticiafurusi2005@gmail.com>
2024-10-21 21:56:48 -0300

    tabela Projeto completa

--- app/Http/Controllers/projetoController.php
+++ app/Http/Controllers/projetoController.php
@@ -24,6 +24,7 @@ class projetoController extends Controller
 
         $projeto = new Projeto();
         $projeto->projeto_nome = $projeto_nome;
+        
         $projeto->projeto_descricao = $projeto_descricao;
         $projeto->projeto_status = $projeto_status;
         $projeto->projeto_data_inicio = $projeto_data_inicio;
@@ -31,14 +32,35 @@ class projetoController extends Controller
         $projeto->save();
         
         $projeto->usuario()->attach($usuario_id);
-       
         return redirect('/projeto');
     }
     public function AlterarProjeto($id){
         $projeto = Projeto::where("id", $id)->first();
         $usuarios = Usuario::with('projeto')->get();
         return view('projeto.alterar', compact('projeto', 'usuarios'));
+    }
 
+    public function SalvarAlteracao(Request $request){
+        $projeto_nome = $request->input("projeto_nome");
+        $usuario_id = $request->input("usuario_id");
+        $projeto_descricao = $request->input("projeto_descricao");
+        $projeto_status = $request->input("projeto_status");
+        $projeto_data_inicio = $request->input("projeto_data_inicio");
+        $projeto_data_fim = $request->input("projeto_data_fim");
+        $id = $request->input("id");
+
+        $projeto = Projeto::where("id", $id)->first();
+
+        $projeto->projeto_nome = $projeto_nome;
+        $projeto->projeto_descricao = $projeto_descricao;
+        $projeto->projeto_status = $projeto_status;
+        $projeto->projeto_data_inicio = $projeto_data_inicio;
+        $projeto->projeto_data_fim = $projeto_data_fim;
+    
+        $projeto->save();
+        $projeto->usuario()->sync   ($usuario_id);
+        return redirect('/projeto');
+        
     }
 
     public function ExcluirProjeto($id){
